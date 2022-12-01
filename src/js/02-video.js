ca661810aa26046ab5fe469ece01b1a91f62eb8f
NovikoffOleh <novikoff10000@gmail.com>
2022-12-01 18:19:08 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,4 @@
-import throttle from 'lodash.throttle';
+/*import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
@@ -9,4 +9,20 @@ function onPlay({ seconds }) {
   localStorage.setItem('videoplayer-current-time', seconds);
 }
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));*/
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const CURRENT_TIME = "videoplayer-current-time";
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+player.on('timeupdate', throttle(onPlay, 1000));
+function onPlay ({ seconds }) {
+    localStorage.setItem(CURRENT_TIME, seconds)
+}
+setCurrentTime()
+function setCurrentTime(){
+    if(!localStorage.getItem(CURRENT_TIME)){
+        return
+    }
+    player.setCurrentTime(localStorage.getItem(CURRENT_TIME))
+}
